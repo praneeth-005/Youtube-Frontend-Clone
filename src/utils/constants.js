5da72a9c5e467c5fb592787c9e02d07a9b8d616d
praneeth-005 <surapureddy.praneeth@gmail.com>
2026-06-06 21:05:10 +0530

    Hide API key in .env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const YOUTUBE_API_KEY = "AIzaSyCeCakPyblb8gbqjkbsE78r1Jklhj2N0Yk";
+export const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=${YOUTUBE_API_KEY}`;
 export const YOUTUBE_COMMENTS_API = (videoId) => `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId=${videoId}&key=${YOUTUBE_API_KEY}`;
 export const RECOMMAND_VIDEOS = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${YOUTUBE_API_KEY}`;
